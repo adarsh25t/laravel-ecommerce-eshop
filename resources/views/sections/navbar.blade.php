b9c2c76e2d83659d448222f768e34614ae171ca8
adarsh25t <adarsh25t@gmail.com>
2022-03-07 22:21:40 +0530

    add the authentication

---
+++ resources/views/sections/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <h3>Eshop</h3>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        <span class="input-group-text" id="basic-addon1"><i class="fas fa-search"></i></span>
+    </div>
+    <div class="nav-actions">
+        <a href="{{ route('login') }}">Login</a>
+    </div>
+</nav>
